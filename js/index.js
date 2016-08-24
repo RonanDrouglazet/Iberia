e7283a7ac8227be32ec93d0f1bd00c0a43e38a56
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2016-08-25 01:14:22 +0200

    allow duplicate date

--- js/index.js
+++ js/index.js
@@ -834,6 +834,46 @@ $(document).ready(function() {
            save()
        }
 
+       window.octoboot_duplicate_date = function(date, duplicate) {
+            if (duplicate) {
+                var now = Date.now()
+                $('section .actu[data-date="' + date.id + '"]').each(function(i, actu) {
+                    var clone = $(actu).clone()
+                    clone
+                        .attr('data-date', now)
+                        .appendTo('section#actualites')
+                    clone
+                        .find('.button')
+                        .click(function() {
+                            moveActu($(this).hasClass('right'))
+                        })
+                    clone
+                        .find('.content')
+                        .click(show_actu_details)
+
+                })
+                $('section#actualites > .dates > .date').removeClass('active')
+                $(date).attr('id', now).addClass('active').click(function() {
+                    selectActusByDate(this.id)
+                })
+                if ($($(date).prev()).children('.line').length === 0) {
+                    $($(date).prev()).append('<div class="line"></div>')
+                }
+                selectActusByDate(now)
+            } else {
+                $('section .actu[data-date="' + date.id + '"]').remove()
+                if ($('section#actualites > .dates > .date').length === 2) {
+                    $('section#actualites > .dates .line').remove()
+                }
+                if ($(date).hasClass('active')) {
+                    setTimeout(function() {
+                        $($('section#actualites > .dates > .date')[0]).addClass('active')
+                        selectActusByDate($('section#actualites > .dates > .date')[0].id)
+                    }, 500)
+                }
+            }
+       }
+
        window.octoboot_duplicate_actu = function(actu, duplicate) {
            if (duplicate) {
                $(actu).find('.button').click(function() {
