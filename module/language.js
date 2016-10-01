d25fa9b7ca2f9c2c2ade488c5657d77bd9dbc427
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2016-10-01 19:06:24 +0200

    update module

--- module/language.js
+++ module/language.js
@@ -1,19 +1,32 @@
 (window.OctoBoot_plugins = window.OctoBoot_plugins || {}).language = function() {
     if (location.hash) {
         var ln = location.hash.substr(1)
+
         var elements = $('.ob-language.' + ln).parent().parent()
-        if (elements.length) {
-            elements.each(function(i, el) {
-                $(el).contents().filter(function() {
-                    return this.nodeType == 3 && !!$(this).text().trim()
-                }).each(function(i, text) {
-                    var sln = $(el).find('.ob-language.' + ln + ' span')[i]
-                    if (sln) {
-                        text.textContent = $(sln).text()
-                    }
-                })
+        elements.each(function(i, el) {
+            $(el).contents().filter(function() {
+                return this.nodeType == 3 && !!$(this).text().trim()
+            }).each(function(i, text) {
+                var sln = $(el).children('.ob-language').find('.ob-language.' + ln + ' span')[i]
+                if (sln) {
+                    text.textContent = $(sln).text()
+                }
             })
-        }
+        })
+
+        var aelements = $('*[ob-language-' + ln + ']'), data
+        aelements.each(function(i, el) {
+            try {
+                data = $(el).attr('ob-language-' + ln).split('&')
+                data.forEach(function(attr) {
+                    attr = attr.split('=')
+                    $(el).attr(attr[0], attr[1])
+                })
+
+            } catch (e) {console.error(e)}
+
+            data = null
+        })
     }
 }
 
